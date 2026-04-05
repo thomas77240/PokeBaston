4c4da5348400b870f8182fd6db277bd51b95fdb2
Doryan <91891883+Doryansio@users.noreply.github.com>
2026-04-05 19:56:26 +0200

    :test_tube: test & CI: add frontend test & update CI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,10 +11,26 @@ export default defineConfig({
       '/api': 'http://localhost:8080', // Redirige les appels API vers Spring
     },
   },
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(),tailwindcss(),tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'react': path.resolve(__dirname, '../node_modules/react'),
+      'react-dom': path.resolve(__dirname, '../node_modules/react-dom'),
+      'react/jsx-runtime': path.resolve(__dirname, '../node_modules/react/jsx-runtime'),
     },
+    dedupe: ['react', 'react-dom'],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    deps: {
+      optimizer: {
+        web: {
+          include: ['react', 'react-dom']
+        }
+      }
+    }
   },
 })
